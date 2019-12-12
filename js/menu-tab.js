29aa7575a5690151ca5412229c5146f220277f32
link2link <link2link@naver.com>
2019-12-12 14:21:57 +0900

    공지사항 및 자료실 디자인 완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -2,6 +2,10 @@ var menu = $('.menu');
 var items = $('.menu-item');
 var btns = $('.btn-menu');
 var anchors = $('.sub-menu a');
+var tabAnchors = $('.board ul a');
+var tabAct = $('.board section');
+var tab = $('.tab');
+
 
 // 토글버튼 클릭했을때 하위 메뉴를 보이도록 설정할 것
 btns.click(function(e){
@@ -11,3 +15,14 @@ btns.click(function(e){
 });
 
 anchors.addClass('icon-dot-cirled');
+tabAnchors.addClass('icon-dot-circled');
+
+/// 탭을 클릭했을 때 클릭한 탭의 목록과 더보기가 보여지도록 설정할 것
+// 마우스와 키보드 모두 사용 가능하도록 설정할 것
+tab.on('click keyup', function(e){
+  e.preventDefault();
+  if(e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13)){
+    tabAct.removeClass('tab-act');
+    $(this).parent().addClass('tab-act');
+  }
+});
